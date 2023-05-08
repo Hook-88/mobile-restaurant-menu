559d19b05c923b681b776fb18444255f7ddd97f9
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-05-08 15:43:43 -0500

    Add logic to hide orderoverview

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import { menuArray } from '/data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 const orderOverview = document.getElementById('order-overview')
 const cartContent = document.getElementById('list-of-orders')
-const userCart = []
+let userCart = []
 
 document.addEventListener('click', function(e) {
   if (e.target.dataset.add) {
     orderOverview.classList.remove('hidden')
     AddProductToCart(e.target.dataset.add)      
+  } else if (e.target.dataset.remove) {
+    let index = userCart.findIndex(function(product){
+      return product.id === Number(e.target.dataset.remove)
+    })
+    userCart.splice(index, 1)
+    renderOrder()
   }
+
 })
 
 function AddProductToCart(productId) {
-  userCart.push(menuArray.filter(function(product){
+  const uniqueProduct = menuArray.filter(function(product){
     return product.id == productId
-  })[0])
+  })[0]
+  userCart.push(uniqueProduct)
   renderOrder()
 }
 
@@ -29,13 +38,15 @@ function renderContentOfCart () {
       html += `
       <li class="product-order">
         <h2>${product.name}</h2>
-        <button class="remove-product" id="remove-product-btn-${product.id}">remove</button>
+        <button class="remove-product" data-remove="${product.id}">remove</button>
         <h4 class="total-price-product">$${product.price}</h4>
       </li>
       `
     })
     cartContent.innerHTML = html
     renderTotalPrice()
+  } else {
+    orderOverview.classList.add('hidden')
   }
 }
 
@@ -76,4 +87,11 @@ function renderProducts() {
 
 renderProducts()
 
+
+
+let index = menuArray.findIndex(function (product) {
+  return product.id === 0
+})
+
+console.log(index)
 
