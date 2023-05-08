cf82e6072cd5785a7e7511898b36334084960fc2
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-05-08 13:16:01 -0500

    Add logic to show order-overview

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 import { menuArray } from '/data.js'
 
-const listOfProducts = document.getElementById('list-of-products')
+const orderOverview = document.getElementById('order-overview')
 
-let htmlElements = ''
+document.addEventListener('click', function(e) {
+  if (e.target.dataset.add) {
+    orderOverview.classList.remove('hidden')
+    
+  }
 
-menuArray.forEach(function(product){
-  
-  htmlElements += `
-  <li class="card">
-    <span class="product-icon">${product.emoji}</span>
-    <div class="product-details">
-        <h2>${product.name}</h2>
-        <p>${product.ingredients}</p>
-        <h4>$${product.price}</h4>
-    </div>
-    <i class="fa-sharp fa-solid fa-circle-plus add-btn"></i>
-  </li>  
-  `
-});
 
-listOfProducts.innerHTML = htmlElements
+})
+
+
+function renderProducts() {
+  const listOfProducts = document.getElementById('list-of-products')
+
+  let htmlElements = ''
+
+  menuArray.forEach(function(product){
+    
+    htmlElements += `
+    <li class="card">
+      <span class="product-icon">${product.emoji}</span>
+      <div class="product-details">
+          <h2>${product.name}</h2>
+          <p>${product.ingredients}</p>
+          <h4>$${product.price}</h4>
+      </div>
+      <i class="fa-sharp fa-solid fa-circle-plus add-btn" data-add="${product.id}"></i>
+    </li>  
+    `
+  });
+
+  listOfProducts.innerHTML = htmlElements
+}
+
+renderProducts()
 
 
